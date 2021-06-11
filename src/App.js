c62dee8b1d04779ad455c7aacb161bbec0dfc705
Kareem Sasa <66285856+kareemsasa3@users.noreply.github.com>
2021-06-11 18:23:37 -0500

    Fixed bug in App.js
    App.js was previously rendering the wrong component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import Main from './components/Main';
 
 const App = () => {
     return (
-        <div className="center-content">
-            <div className="ui inverted secondary button" tabindex="0">
-                <div className="visible content">Click to Enter</div>
-                <div className="hidden content">
-                    <i className="right arrow icon"></i>
-                </div>
-            </div> 
-        </div>
+        <Main />
     );
 };
 
